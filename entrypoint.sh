d9ada9f1d8098488f35a1e4579fe8f4e121b7709
Abnash Thahir <64632291+Abnashthahir@users.noreply.github.com>
2026-01-11 23:10:18 +0530

    9

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,15 @@ cd /home/frappe/frappe-bench
 if [ ! -d "sites/$SITE_NAME" ]; then
   echo "Creating site $SITE_NAME"
 
-  bench new-site $SITE_NAME \
+    bench new-site "$SITE_NAME" \
     --admin-password "$ADMIN_PASSWORD" \
     --db-host "$DB_HOST" \
     --db-name "$DB_NAME" \
     --db-password "$DB_PASSWORD" \
+    --db-root-username "$DB_USER" \
+    --db-root-password "$DB_PASSWORD" \
     --mariadb-user-host-login-scope='%'
+
 
   bench --site $SITE_NAME install-app crm
 fi
