cfcfbf058150a11d883bd7a53141c8bedcea807c
Abnash Thahir <64632291+Abnashthahir@users.noreply.github.com>
2026-01-11 23:05:54 +0530

    9

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,11 @@ if [ ! -d "sites/$SITE_NAME" ]; then
   echo "Creating site $SITE_NAME"
 
   bench new-site $SITE_NAME \
-    --admin-password $ADMIN_PASSWORD \
-    --db-host $DB_HOST \
-    --db-name $DB_NAME \
-    --db-password $DB_PASSWORD \
-    --no-mariadb-socket
+    --admin-password "$ADMIN_PASSWORD" \
+    --db-host "$DB_HOST" \
+    --db-name "$DB_NAME" \
+    --db-password "$DB_PASSWORD" \
+    --mariadb-user-host-login-scope='%'
 
   bench --site $SITE_NAME install-app crm
 fi
